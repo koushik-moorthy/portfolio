3b96d6fffed2f8d073c4a4b1924fb75fa09c8dea
koushikd <koushik.d@presidio.com>
2021-05-23 15:33:59 +0530

    Responsiveness added

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ window.addEventListener('scroll',function(){
 	var header = document.querySelector('header');
 	header.classList.toggle('sticky',window.scrollY > 0);
 })
+
+var hamburger = document.getElementById("hamburger");
+var navUL = document.getElementById("nav-ul");
+
+hamburger.addEventListener('click',()=>{
+	navUL.classList.toggle('show');
+});
