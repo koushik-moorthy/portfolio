f05f23d5f89ccae38b22fcf62de06352357de2cf
koushikd <koushik.d@presidio.com>
2021-05-29 20:10:46 +0530

    Test commit

--- script.js
+++ script.js
@@ -9,3 +9,8 @@ var navUL = document.getElementById("nav-ul");
 hamburger.addEventListener('click',()=>{
 	navUL.classList.toggle('show');
 });
+
+
+$(window).on("load",function(){
+	$(".loader-wrapper").fadeOut("slow");
+});
